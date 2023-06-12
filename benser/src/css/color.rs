97dbf416bcc772ca88357e68625e76550318ab1a
Ben Cook <mcbeanoo@gmail.com>
2023-06-12 16:47:58 +1000

    start fixing colors (#6)

--- benser/src/css/color.rs
+++ benser/src/css/color.rs
@@ -10,8 +10,4 @@ impl Color {
     pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
         Color { r, g, b, a }
     }
-
-    pub fn as_float(&self) -> [f32; 4] {
-        [self.r.into(), self.g.into(), self.b.into(), self.a.into()]
-    }
 }
